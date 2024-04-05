3448418f1c54b6a27439f23559253faeaade9f49
Martin Keßel <martin.kessel@funkemedien.de>
2024-04-05 08:49:18 +0200

    Deleting module-info.java.

--- events/src/main/java/de/kessel/events/dto/EventRequestDto.java
+++ events/src/main/java/de/kessel/events/dto/EventRequestDto.java
@@ -1,6 +1,5 @@
 package de.kessel.events.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -16,9 +15,7 @@ public class EventRequestDto {
 
     @NotBlank(message = "Text should not be empty.")
     @Schema(description = "Text of the Event", example = "Hallo")
-    @JsonProperty("text")
     String text;
     @Schema(description = "Translations of the Event", example = "{\"en\": \"hello\", \"fr\": \"bonjour\"}")
-    @JsonProperty("translations")
     List<TranslationRequestDto> translations;
 }
--- events/src/main/java/de/kessel/events/dto/TranslationRequestDto.java
+++ events/src/main/java/de/kessel/events/dto/TranslationRequestDto.java
@@ -1,14 +1,11 @@
 package de.kessel.events.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class TranslationRequestDto {
-    @JsonProperty("language")
     private String language;
-    @JsonProperty("text")
     private String text;
 }
